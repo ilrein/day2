2e3e9f76e9140577b44bf6f47ff94a71650ae02d
Ilia <ilia.reingold@gmail.com>
2014-03-25 13:01:42 -0400

    tip code and the rest of it

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+puts "Meal price?: "
+input = gets.chomp.to_i
+
+n = input * 1.15
+puts "the ideal total + tip is: $" + n.to_s
+
+puts "by the way, the output of 45628 * 7839 is: "
+puts 45628 * 7839
